118f3dc2cd4b8d601c6dcbd87896914d04c83b4b
JungJuyeon81 <juyeoni0801@gmail.com>
2023-11-23 15:30:10 +0900

    ottalk_00

---
+++ app/src/main/java/com/example/ottalk/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.ottalk;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        drawerLayout=findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Toolbar toolbar = findViewById(R.id.modified_toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.homeFragment);
+        }
+
+        replaceFragment(new HomeFragment());
+
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.ranking:
+                        replaceFragment(new RankingFragment());
+                        break;
+                    case R.id.mypage:
+                        replaceFragment(new MypageFragment());
+                        break;
+                    case R.id.wishlist:
+                        replaceFragment(new WishlistFragment());
+                        break;
+                    case R.id.shelf:
+                        replaceFragment(new ShelfFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+
+        Button HomeButton = findViewById(R.id.toolbar_logo);
+        HomeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                replaceFragment(new HomeFragment());
+                bottomNavigationView.setSelectedItemId(R.id.homeFragment);
+            }
+        });
+    }
+
+
+    public void setSupportActionBar(Toolbar toolbar) {
+    }
+    //Outside onCreate
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
+
